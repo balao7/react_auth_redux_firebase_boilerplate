488b71cd4d36b4f5f78e8787cff9156a2a361b2a
Fidalgo <pedrofidalgo7@gmail.com>
2019-04-30 23:12:04 +0100

    added custom hook to prevent scroll on page when popup is opened

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import styles from './Modal.module.css';
 import Backdrop from '../backdrop/Backdrop';
 
-const Modal = ({ opened, close, children }) =>
-  ReactDOM.createPortal(
+const Modal = ({ opened, close, children }) => {
+  useLockBodyScroll(opened);
+
+  return ReactDOM.createPortal(
     <>
       <Backdrop opened={opened} close={close} open />
       <div className={`${styles.Modal} ${opened ? styles.Opened : null}`}>
@@ -14,5 +16,19 @@ const Modal = ({ opened, close, children }) =>
     </>,
     document.getElementById('modal-root')
   );
+};
+
+// Hook to prevent scroll when its opened
+function useLockBodyScroll(opened) {
+  useLayoutEffect(() => {
+    if (!opened) return;
+    // Get original body overflow
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    // Prevent scrolling on mount
+    document.body.style.overflow = 'hidden';
+    // Re-enable scrolling when component unmounts
+    return () => (document.body.style.overflow = originalStyle);
+  }, [opened]); // Empty array ensures effect is only run on mount and unmount
+}
 
 export default Modal;
